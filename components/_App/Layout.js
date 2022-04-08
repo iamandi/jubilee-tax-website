ae243547e6ebfd31695911a543262e290080d396
iamandi <aakawade9@gmail.com>
2022-04-07 19:27:13 -0700

    Add font optimization nextjs way

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -11,6 +11,12 @@ const Layout = ({ children }) => {
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>Jubilee Tax and Financial Solutions, Inc</title>
+                <meta name="description" content="For saving, growing, and spending your money and protecting the things you love. We offer expert-driven advice and tools that help you make the best personal and business finance decisions." />
+                <meta name="author" content="Krotion Tech" />
+                <link
+                    href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+                    rel="stylesheet"
+                />
             </Head>
 
             {children}
