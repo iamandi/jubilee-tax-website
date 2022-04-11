04fd7a302a0a61956483795b70eeb5717bda963e
iamandi <aakawade9@gmail.com>
2022-04-11 00:17:01 -0700

    Test font loading2

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
         return (
             <Html dir={dir} lang={locale}>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                     <link
                         as="style"
@@ -21,7 +21,10 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
                         type="text/css"
                         crossorigin
-                    />
+                    /> */}
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
                     <link rel="icon" type="image/vnd.microsoft" href="/images/favicon.ico"></link>
                 </Head>
                 <body>
