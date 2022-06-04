be48d1f2b84ad811d43f1ff031b450f1067339b4
iamandi <aakawade9@gmail.com>
2022-06-04 13:46:50 -0700

    Temporary fix to siteKey error

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -84,12 +84,6 @@ const ContactForm = () => {
 
                     <div className="col-lg-6 col-md-12">
                         <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
-                            <ReCAPTCHA
-                                ref={recaptchaRef}
-                                size="invisible"
-                                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-                                onChange={onReCAPTCHAChange}
-                            />
                             <div className="row">
                                 <div className="col-lg-12 col-md-12">
                                     <div className="form-group">
